412e039bfc0f529180d5654d39ad29bbcd6bb6a4
paulk <paul.kilpatrick@finance.gov.au>
2015-07-13 10:16:03 +1000

    fix footer

---
+++ ona_theme/templates/region/region--footer-bottom.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the footer region.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728140
+ */
+?>
+<?php if ($content): ?>
+  <div id="footer_bottom" class="<?php print $classes; ?> clearfix">
+    <?php print $content; ?>
+  </div>
+<?php endif; ?>
