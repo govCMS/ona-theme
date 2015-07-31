757c59d4d1828ae1b4934ec466695c6042f4ac84
paulkilla <paul.killer@gmail.com>
2015-07-31 10:55:36 +1000

    test out new js

--- agency_whitesite/template.php
+++ agency_whitesite/template.php
@@ -563,3 +563,31 @@ function agency_whitesite_preprocess_node(&$vars, $hook) {
                        "<span class='postedDate'>on " . date("l, M jS, Y", $vars['created']) . " </span>".
                        "<span class='postedBy'>by " . $vars['name'] ."</span>";
 }
+
+/**
+* Implement hook_js_alter()
+* Attempt to replace the system jQuery on non admin and non node admin pages with a newer version provided by the theme
+*/
+
+function agency_whitesite_js_alter(&$javascript) {
+$node_admin_paths = array(
+'node/*/edit',
+'node/add',
+'node/add/*',
+'node/*/extend_review_date',
+);
+$replace_jquery = TRUE;
+if (path_is_admin(current_path())) {
+$replace_jquery = FALSE;
+} else {
+foreach ($node_admin_paths as $node_admin_path) {
+if (drupal_match_path(current_path(), $node_admin_path)) {
+$replace_jquery = FALSE;
+}
+}
+}
+// Swap out jQuery to use an updated version of the library.
+if ($replace_jquery) {
+$javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'agency_whitesite') . '/js/vendor/jquery.min.js';
+}
+}
